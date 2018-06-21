1bab82e11099f5f78ee611ee9aab5a429ff876a0
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 00:14:34 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-      array[1..2].sort
+      temp = array[1]
+      array[1] = array[2]
+      array[2] = temp
 end
 
 def reverse_array(array)
