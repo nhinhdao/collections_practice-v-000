c2c5cbd7951aac4b9cdb72c3bb80001d4da6cae8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 00:32:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+      array.sort
+end
 
+def sort_array_desc(array)
+      array.sort.reverse
+end
+
+def sort_array_char_count(array)
+      array.sort do |a,b|
+            a.length <=> b.length
+      end
+end
+
+def swap_elements(array)
+      temp = array[1]
+      array[1] = array[2]
+      array[2] = temp
+      array
+end
+
+def reverse_array(array)
+      array.reverse
+end
+
+def kesha_maker(array)
+      array.each{ |chr|  chr[2] = '$'}
+end
+
+def find_a(array)
+      array.select {|w| w[0] == 'a'}
+end
+
+def sum_array(array)
+      sum = 0
+      array.each {|i| sum += i}
+      sum
+end
+
+def add_s(array)
+      array.each { |e|  e  << 's' if array.index(e) != 1}
+end
