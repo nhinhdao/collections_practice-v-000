e97eb6a9251cb0776928a49ffe59b6b1134363df
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 00:07:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+      array.sort
+end
 
+def sort_array_desc(array)
+      array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+      
+end
+
+def swap_elements
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a
+
+end
+
+def sum_array
+end
+
+def add_s
+end
